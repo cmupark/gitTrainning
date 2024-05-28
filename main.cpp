212a1eaefeb6d32ac0461deeb8c5b5d55e872c0e
sangsoon98.park <sangsoon98.park@lge.com>
2024-05-28 10:37:55 +0900

    update Todos

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+    std::cout << "FIND YOUR PATH\n" << std::endl;
+
+    /*
+        TODO
+        
+        inputMaze() :           input initial maze. the maximum size of the maze is 100x100
+                                0 is the available section to go. 1 is the wall that you can't go.
+        
+        findShortestPath() :    find the shortest path by bfs algorithm.
+
+        printMaze():            print the shortest path. you must categorize the shotest path from others.
+    */
+   
+	//inputMaze();
+    //findShortestPath();
+    //printMaze();
+
+    std::cout << "This is the shortest path of your maze.";
+    return 0;
+}
